5411c7c5342397be61b4f5f97ad14180aa901599
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-05-30 12:19:33 +0630

    project setup and run

---
+++ xero/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='width-full h-screen flex justify-center items-center'>
+      <p className="text-4xl text-red-700 font-bold">
+        Hello world from Responsive Dashboard Design Project!
+      </p>
+    </div>
+  )
+}
+
+export default App
